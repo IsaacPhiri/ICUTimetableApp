7d3bccb82b43fe3c787d3497685a33152f5085e7
Isaac Phiri <isaacphiri315@gmail.com>
2024-12-24 15:41:41 +0200

    SplashScreen feature added

---
+++ app/src/main/java/com/example/icutimetableapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.icutimetableapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+    }
+}
