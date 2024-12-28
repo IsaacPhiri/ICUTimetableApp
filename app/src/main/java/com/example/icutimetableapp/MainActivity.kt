150e1242e411b11e95c610430c8d67b9bc2beec1
Isaac Phiri <isaacphiri315@gmail.com>
2024-12-28 04:22:16 +0200

    Added: Login feature

--- app/src/main/java/com/example/icutimetableapp/MainActivity.kt
+++ app/src/main/java/com/example/icutimetableapp/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.example.icutimetableapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.Button
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(1000)
         installSplashScreen()
         setContentView(R.layout.activity_login)
-        //setContentView(R.layout.activity_main)
+       // setContentView(R.layout.activity_main)
+
+        //auth = FirebaseAuth.getInstance()
+
+        //val logoutButton: Button = findViewById(R.id.logoutButton)
+
+        //logoutButton.setOnClickListener {
+         //   auth.signOut()
+        //    startActivity(Intent(this, LoginActivity::class.java))
+       //     finish()
+       // }
     }
 }
