64d8cc5012fa71d31df5b8998b152467a52a4fc3
Isaac Phiri <isaacphiri315@gmail.com>
2025-01-04 02:15:05 +0200

    Add RetrofitInstance for network operations

---
+++ app/src/main/java/com/example/icutimetableapp/RetrofitInstance.kt
@@ -0,0 +1,17 @@
+package com.example.icutimetableapp
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    fun <T> create(service: Class<T>): T {
+        return retrofit.create(service)
+    }
+}
